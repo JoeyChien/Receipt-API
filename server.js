8c8e0211f3b3585a04cb3d98d82510e490eaf878
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-02-28 16:02:55 +0800

    套件清單(安裝express,body-parser,cors,pg,pg-hstore,sequelize)以及建立server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+// Bring in required Modules
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Bring in the route
+const routes = require("./app/routes");
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+app.use("/api/receipts", routes);
+
+// Define PORT
+const PORT = process.env.PORT || 8080;
+
+// Listen to the defined PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+process.on('unhandledRejection', error => {
+  console.error('unhandledRejection', error);
+  process.exit(1) // To exit with a 'failure' code
+});
