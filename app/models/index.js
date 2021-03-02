618ca9b3c48f3d13b10a22c7170b39086ed671af
Joey <aileen60222@gmail.com>
2021-03-02 16:54:54 +0800

    修改db連線部分，如有process.env.DATABASE_URL參數則連heroku的db

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,29 @@
 const dbConfig = require("../config/db.config");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorAliases: 0,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres         database
+  sequelize =new Sequelize(process.env.DATABASE_URL,
+  {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true,
+    }
+  });
+} else {
+  const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorAliases: 0,
+  
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  });
+}  
 
 const db = {};
 db.Sequelize = Sequelize;
